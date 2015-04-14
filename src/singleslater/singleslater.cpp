5a56e4f72e313f79393623cc393a2857cc1e1e66
David Williams-Young <dbwy@uw.edu>
2015-04-14 09:45:22 -0700

    Changed energy output from a.u. to Eh

--- src/singleslater/singleslater.cpp
+++ src/singleslater/singleslater.cpp
@@ -157,10 +157,10 @@ void SingleSlater::computeEnergy(){
 //--------------------//
 void SingleSlater::printEnergy(){
   this->fileio_->out<<"\nEnergy Information:"<<endl;
-  this->fileio_->out<<std::setw(30)<<"E(one electron) = "<<std::setw(15)<<this->energyOneE<<std::setw(5)<<" a.u. "<<endl;
-  this->fileio_->out<<std::setw(30)<<"E(two electron) = "<<std::setw(15)<<this->energyTwoE<<std::setw(5)<<" a.u. "<<endl;
-  this->fileio_->out<<std::setw(30)<<"E(nuclear repulsion) = "<<std::setw(15)<<this->energyNuclei<<std::setw(5)<<" a.u. "<<endl;
-  this->fileio_->out<<std::setw(30)<<"E(total) = "<<std::setw(15)<<this->totalEnergy<<std::setw(5)<<" a.u. "<<endl;
+  this->fileio_->out<<std::setw(30)<<"E(one electron) = "<<std::setw(15)<<this->energyOneE<<std::setw(5)<<" Eh "<<endl;
+  this->fileio_->out<<std::setw(30)<<"E(two electron) = "<<std::setw(15)<<this->energyTwoE<<std::setw(5)<<" Eh "<<endl;
+  this->fileio_->out<<std::setw(30)<<"E(nuclear repulsion) = "<<std::setw(15)<<this->energyNuclei<<std::setw(5)<<" Eh "<<endl;
+  this->fileio_->out<<std::setw(30)<<"E(total) = "<<std::setw(15)<<this->totalEnergy<<std::setw(5)<<" Eh "<<endl;
 };
 //-------------------------//
 // form the density matrix //
