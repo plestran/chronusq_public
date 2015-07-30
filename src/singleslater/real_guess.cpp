a84c10cb6ce3dd97c24372ed413596b16090f289
David Williams-Young <dbwy@uw.edu>
2015-07-30 16:07:52 -0700

    Fixed UHF bug by spin averaging both alpha and beta densities

--- src/singleslater/real_guess.cpp
+++ src/singleslater/real_guess.cpp
@@ -101,13 +101,10 @@ void SingleSlater<double>::formGuess() {
 
     uniqueAtom.readCharge(0);
     uniqueAtom.readMultip(uniqueElement[iUn].defaultMult);
-  //uniqueAtom.printInfo(this->fileio_,this->controls_);
 
-//  basisSetAtom.constructLocal(&uniqueAtom);
     this->basisset_->constructExtrn(&uniqueAtom,&basisSetAtom);
     basisSetAtom.makeMapSh2Bf();
     basisSetAtom.makeMapSh2Cen(&uniqueAtom);
-  //basisSetAtom.printInfo();
     basisSetAtom.renormShells();
 
     controlAtom.iniControls();
@@ -132,150 +129,26 @@ void SingleSlater<double>::formGuess() {
       auto iSize = this->basisset_->mapCen2Bf(iAtm)[1]; 
       this->densityA_->block(iBfSt,iBfSt,iSize,iSize)= (*hartreeFockAtom.densityA_);
       if(!this->isClosedShell){
-//      this->densityA_->block(iBfSt,iBfSt,iSize,iSize) *= (double)nAE_/(double)(nAE_+nBE_);
         if(hartreeFockAtom.isClosedShell)
-          this->densityB_->block(iBfSt,iBfSt,iSize,iSize)= (*hartreeFockAtom.densityA_);
+          this->densityB_->block(iBfSt,iBfSt,iSize,iSize)= 2*(*hartreeFockAtom.densityA_);
         else
-          this->densityB_->block(iBfSt,iBfSt,iSize,iSize)= (*hartreeFockAtom.densityB_);
-//      this->densityB_->block(iBfSt,iBfSt,iSize,iSize) *= (double)nBE_/(double)(nAE_+nBE_);
+          this->densityB_->block(iBfSt,iBfSt,iSize,iSize)= 
+            (*hartreeFockAtom.densityB_) + (*hartreeFockAtom.densityA_);
       } else {
         if(!hartreeFockAtom.isClosedShell){
           this->densityA_->block(iBfSt,iBfSt,iSize,iSize) += (*hartreeFockAtom.densityB_);
-//        this->densityA_->block(iBfSt,iBfSt,iSize,iSize) *= 0.5;
         }
       }
     }
 
   }
-    if(!this->aointegrals_->haveAOOneE) this->aointegrals_->computeAOOneE();
-    this->haveMO = true;
-    this->haveDensity = true;
-//  CErr();
-/*
-  for(auto iUn = 0; iUn < uniqueElement.size(); iUn++){
-    auto aointegralsAtom  = new AOIntegrals();
-    auto hartreeFockAtom  = new SingleSlater<double>();
-    auto controlAtom      = new Controls();
-    auto basisSetAtom     = new BasisSet(*this->basisset_);
-    auto dfBasissetAtom   = new BasisSet();
-    std::vector<libint2::Shell> atomShell;
-    Molecule uniqueAtom(uniqueElement[iUn],this->fileio_);
-    uniqueAtom.readCharge(0);
-    uniqueAtom.readMultip(uniqueElement[iUn].defaultMult);
-
-//  this->basisset_->constructExtrn(&uniqueAtom,basisSetAtom); 
-    basisSetAtom->constructLocal(&uniqueAtom);
-    basisSetAtom->makeMapSh2Bf();
-    basisSetAtom->makeMapSh2Cen(&uniqueAtom);
-    basisSetAtom->printInfo();
-    auto nsize = basisSetAtom->nBasis();
-
-    RealMatrix denMOA(nsize, nsize);
-    RealMatrix denMOB(nsize, nsize);
-    controlAtom->iniControls();
-    controlAtom->doCUHF = true;
-    aointegralsAtom->iniAOIntegrals (&uniqueAtom, basisSetAtom, this->fileio_, controlAtom, dfBasissetAtom);
-    hartreeFockAtom->iniSingleSlater(&uniqueAtom, basisSetAtom, aointegralsAtom, this->fileio_, controlAtom);
-    hartreeFockAtom->moA_->setZero();
-    if (hartreeFockAtom->Ref_ != RHF) hartreeFockAtom->moB_->setZero();
-    hartreeFockAtom->haveMO = true;
-    cout << this << endl << aointegralsAtom << endl << hartreeFockAtom << endl << basisSetAtom << endl;
-    cout << "HERE" << endl;
-    aointegralsAtom->computeSchwartz();
-    CErr();
-    hartreeFockAtom->formFock();
-    cout << "HERE" << endl;
-    hartreeFockAtom->computeEnergy();
-    cout << "HERE" << endl;
-    hartreeFockAtom->SCF();
-    cout << "HERE" << endl;
-    if (hartreeFockAtom->Ref_ != RHF){
-      denMOB = (*hartreeFockAtom->densityB_);
-      denMOA = (*hartreeFockAtom->densityA_);
-    }
-    else{
-      denMOA = (*hartreeFockAtom->densityA_);
-      denMOB = (*hartreeFockAtom->densityA_);
-    }
-    atomMO.push_back(denMOA);
-    atomMOB.push_back(denMOB);
-  //basisSetAtom->setnBasis(0);
-  //basisSetAtom->setnPrimitive(0);
-  }
-  this->fileio_->out << endl << "Atomic SCF Completed............." << endl << endl;
-  int n = (*this->moA_).rows();
-  int lenX  = n * n;
-  int lenFp = n * n;
-  int lenFb = n * n;
-  int lwork = 4 *n;
-  int lenoccNum = n;
-  int lenScr    = lenX + lenFp + lenFb + lwork + lenoccNum;
-  double *SCR, *Xmem, *Fpmem, *Fbmem, *work, *occNum;
-  
-  SCR   = new double[lenScr];
-  Xmem  = SCR;
-  Fpmem = Xmem  + lenX;
-  Fbmem = Fpmem + lenFp;
-  work  = Fbmem + lenFb;
-  occNum= work  + lwork;
- 
-  RealMap X(Xmem, n, n);
-  RealMap FpAlpha(Fpmem, n, n);
-  RealMap FpBeta (Fbmem, n, n);
-  for(auto iUn = 0; iUn < uniqueElement.size(); iUn++){
-    Molecule uniqueAtom(uniqueElement[iUn],this->fileio_);
+  if(!this->isClosedShell){
+    int nE = this->molecule_->nTotalE();
+    (*this->densityA_) *= (double)this->nAE_/(double)nE ;
+    (*this->densityB_) *= (double)this->nBE_/(double)nE ;
   }
   this->haveMO = true;
-  if(!this->aointegrals_->haveAOOneE) this->aointegrals_->computeAOOneE();
-  X=(*this->aointegrals_->overlap_).pow(-0.5);
-  int nIndex=0;
-  for (auto k=0; k<repeatedAtoms.size();k++){
-    for (auto i=nIndex; i<atomMO[repeatedAtoms[k]].rows()+nIndex;i++){
-      for (auto j=nIndex; j<atomMO[repeatedAtoms[k]].cols()+nIndex;j++){
-	(*this->densityA_)(i,j)=((atomMO[repeatedAtoms[k]])(i-nIndex,j-nIndex)+(atomMOB[repeatedAtoms[k]])(i-nIndex,j-nIndex))/2;         
-        if (this->Ref_ != RHF){
-	  (*this->densityB_)(i,j)=((atomMO[repeatedAtoms[k]])(i-nIndex,j-nIndex)+(atomMOB[repeatedAtoms[k]])(i-nIndex,j-nIndex))/2;         
-	}
-      }
-    }
-    nIndex = nIndex+atomMO[repeatedAtoms[k]].rows();
-  }
-  atomMO.resize(0);
-  atomMOB.resize(0);
-  
-  if(this->Ref_ != RHF){
-    int numE = this->molecule_->nTotalE();
-    int betaE  = (numE-this->molecule_->multip()-1)/2;
-    int alphaE = (numE - betaE);
-    (*this->densityA_) =2* (double)alphaE/(double)numE * (*this->densityA_);
-    (*this->densityB_) =2* (double)betaE/(double)numE * (*this->densityB_);
-  }
-  int info  = -1;
-  char j = 'V';
-  char u = 'U';
   this->haveDensity = true;
-  this->formFock();
-  FpAlpha   = X.transpose()*(*this->fockA_)*X;
-  if(this->Ref_ != RHF) {
-    FpBeta     = X.transpose()*(*this->fockB_)*X;
-  }
-  dsyev_(&j,&u,&n, FpAlpha.data(), &n, this->epsA_->data(), work, &lwork, &info);
-  FpAlpha.transposeInPlace(); // bc Row Major
-  (*this->moA_) = X*FpAlpha;
-  
-  if(this->Ref_ != RHF) {
-    dsyev_(&j,&u,&n, FpBeta.data(), &n, this->epsB_->data(), work, &lwork, &info);
-    FpBeta.transposeInPlace(); // bc Row Major
-    (*this->moB_) = X*FpBeta;
-  }
-
-  this->haveDensity = false;
-  if(this->controls_->printLevel>=3) {
-    prettyPrint(this->fileio_->out,(*this->moA_),"Alpha MO Coeff");
-    if(this->Ref_ != RHF) prettyPrint(this->fileio_->out,(*this->moB_),"Beta MO Coeff");
-  };
-  delete [] SCR;
-*/
 };
 //------------------------------------------//
 // form the initial guess of MOs from input //
