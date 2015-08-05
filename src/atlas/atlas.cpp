39a561bf5a290876b8449584d8d27fb887a1b4ca
David Williams-Young <dbwy@uw.edu>
2015-08-05 10:42:19 -0700

    Added GHF keyword to toggle TCS flag. This sucessfully separates GHF from RHF/UHF/CUHF in test cases. Note that there is a bug for the GHF keyword in that it must be specified BEFORE the basisset definition or things will not work (maps are incorrect)

--- src/atlas/atlas.cpp
+++ src/atlas/atlas.cpp
@@ -55,7 +55,7 @@ int ChronusQ::atlas(int argc, char *argv[], GlobalMPI *globalMPI) {
 
   // Initialize default settings and read input
   controls->iniControls();
-  controls->doTCS = true;
+//controls->doTCS = true;
   readInput(fileIO.get(),molecule.get(),basisset.get(),controls.get(),dfBasisset.get());
 //  fileIO->iniFileIO(controls->restart);
 
