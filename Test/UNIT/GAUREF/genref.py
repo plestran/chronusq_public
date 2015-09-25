1a2f8a1a31530ffa4421bb2003d24a9e83a658a1
David Williams-Young <dbwy@uw.edu>
2015-09-25 14:05:16 -0700

    Generating the reference values for RESP class of UT

--- Test/UNIT/GAUREF/genref.py
+++ Test/UNIT/GAUREF/genref.py
@@ -1,5 +1,6 @@
 import sys,os
 from grabinfo_SCF import *
+from grabinfo_RESP import *
 
 def genTable():
 	start = False
@@ -11,12 +12,28 @@ def genTable():
 			start = True
 			continue
 		if start:
-			table.append([strx[0],strx[1].replace('.com','.log')])
+			table.append([strx[0],strx[1],strx[2].replace('.com','.log')])
 	return table
+
+class UnitTestNotFound(Exception):
+	def __init__(self,value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
 
 def genRef():
 	table = genTable()
 	
 	of = open("ref.val",'w')
 	for i in range(len(table)):
-		of.write(table[i][0]+'/'+grabinfoSCF(table[i][1])+'\n')
+		of.write(table[i][0]+'/')
+		if table[i][1] == 'SCF':
+			of.write(grabinfoSCF(table[i][2]))
+		elif table[i][1] == 'RESP':
+			of.write(grabinfoRESP(table[i][2]))
+		else:
+			raise UnitTestNotFound(table[i][1])	
+		of.write('\n')
+
+if __name__ in "__main__":
+	genRef()
