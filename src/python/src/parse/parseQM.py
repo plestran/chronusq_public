plestran/chronusq_public

commit a9c7cafa9a740579391dc8fe017193f1a7ff70ef

Various cleaning, added a check for required keywords in each section parse function

src/python/src/parse/parseQM.py
@@ -1,6 +1,7 @@
 import os,sys
 import libpythonapi as chronusQ
 from parseBasis import parseBasis
+from meta.knownKeywords import requiredKeywords
 #from standardJobs import *
 
 #
@@ -26,10 +27,19 @@ def parseQM(workers,secDict):
 #
 # Check for unknown keywords in the QM section
 #
-  knownKeywords = [ 'reference', 'basis', 'job' ]
-  for i in ssSettings:
-    if i not in knownKeywords:
-      print "Keyword QM."+ str(i) +" not recognized"
+# knownKeywords = [ 'reference', 'basis', 'job' ]
+# for i in ssSettings:
+#   if i not in knownKeywords:
+#     print "Keyword QM."+ str(i) +" not recognized"
+
+#
+#  Check that all of the required keywords for Molecule
+#  object are found
+#
+  for i in requiredKeywords['QM']:
+    if i not in ssSettings:
+      print 'Required keyword QM.' + str(i) + ' not found'
+      exit(1)
 
 #
 # Try to set the reference for CQ::SingleSlater
@@ -65,20 +75,20 @@ def parseQM(workers,secDict):
 #
 def handleReference(workers,ref):
   mult = workers["CQMolecule"].multip()
-  if ref in ('HF','hf'):
+  if ref in ('hf'):
     if mult == 1:
       workers["CQSingleSlaterDouble"].setRef(chronusQ.Reference.RHF)
       workers["CQSingleSlaterDouble"].isClosedShell = True
     else:
       workers["CQSingleSlaterDouble"].setRef(chronusQ.Reference.UHF)
-  elif ref in ('RHF','rhf'):
+  elif ref in ('rhf'):
     workers["CQSingleSlaterDouble"].setRef(chronusQ.Reference.RHF)
     workers["CQSingleSlaterDouble"].isClosedShell = True
-  elif ref in ('UHF','uhf'):
+  elif ref in ('uhf'):
     workers["CQSingleSlaterDouble"].setRef(chronusQ.Reference.UHF)
-  elif ref in ('CUHF','cuhf'):
+  elif ref in ('cuhf'):
     workers["CQSingleSlaterDouble"].setRef(chronusQ.Reference.CUHF)
-  elif ref in ('GHF','ghf'):
+  elif ref in ('ghf'):
     workers["CQSingleSlaterDouble"].setRef(chronusQ.Reference.TCS)
 
   TCMethods = [chronusQ.Reference.TCS, chronusQ.Reference.GKS]
@@ -86,13 +96,13 @@ def handleReference(workers,ref):
     workers["CQSingleSlaterDouble"].setNTCS(2)
 
 def parseRT(workers,settings):
-  requiredKeywords = []
-  optionalKeywords = [ 'maxstep' , 'timestep' , 'edfield' , 'time_on',
-                       'time_off', 'frequency', 'phase'   , 'sigma'  ,
-                       'envelope', 'ortho'    , 'iniden'  , 'uprop'  ]
-  knownKeywords = requiredKeywords + optionalKeywords
+# requiredKeywords = []
+# optionalKeywords = [ 'maxstep' , 'timestep' , 'edfield' , 'time_on',
+#                      'time_off', 'frequency', 'phase'   , 'sigma'  ,
+#                      'envelope', 'ortho'    , 'iniden'  , 'uprop'  ]
+# knownKeywords = requiredKeywords + optionalKeywords
 
-  reqMap = {}
+# reqMap = {}
   optMap = {   'maxstep':workers['CQRealTime'].setMaxSteps ,
               'timestep':workers['CQRealTime'].setStepSize ,
                'edfield':workers['CQRealTime'].setFieldAmp ,
@@ -106,40 +116,11 @@ def parseRT(workers,settings):
                 'iniden':workers['CQRealTime'].setInitDen  ,
                  'uprop':workers['CQRealTime'].setFormU    }
 
-# orthoMap = {    'lowdin':chronusQ.RealTime_ORTHO.Lowdin    ,
-#               'cholesky':chronusQ.RealTime_ORTHO.Cholesky  ,
-#              'canonical':chronusQ.RealTime_ORTHO.Canonical }
-
-# formUMap = { 'eigendecomp':chronusQ.RealTime_FORM_U.EigenDecomp ,
-#                   'taylor':chronusQ.RealTime_FORM_U.Taylor      }
-
-# envMap   = {       'pw':chronusQ.RealTime_ENVELOPE.Constant ,
-#               'linramp':chronusQ.RealTime_ENVELOPE.LinRamp  ,
-#              'gaussian':chronusQ.RealTime_ENVELOPE.Gaussian ,
-#                  'step':chronusQ.RealTime_ENVELOPE.Step     ,
-#                 'sinsq':chronusQ.RealTime_ENVELOPE.SinSq    }
-
-# for i in settings:
-#   if i not in knownKeywords:
-#     print "Keyword RealTime."+ str(i) +" not recognized"
-#   elif i in ('maxstep','MaxStep','MAXSTEP','iniden','IniDen','INIDEN'):
-#     settings[i] = int(settings[i])
-#   elif i in ('ortho','Ortho','ORTHO'):
-#     settings[i] = orthoMap[i]
-#   elif i in ('envelope','Envelope','ENVELOPE'):
-#     settings[i] = envMap[i]
-#   elif i in ('uprop','Uprop','UProp','UPROP'):
-#     settings[i] = formUMap[i]
-#   elif i in ('edfield','EDfield','EDField'):
-#     settings[i] = settings[i].split()
-#     for j in range(len(settings[i])): settings[i][j] = float(settings[i][j])
-#   else:
-#     settings[i] = float(settings[i])
 
 
   for i in optMap:
     try:
-      if i not in ('edfield','EDfield','EDField'):
+      if i not in ('edfield'):
         optMap[i](settings[i])
       else:
         optMap[i](settings[i][0],settings[i][1],settings[i][2])

src/python/src/parse/meta/knownKeywords.py
@@ -34,3 +34,12 @@ knownKeywords['RT'] = {
 }
 
 
+requiredKeywords = {}
+requiredKeywords['Molecule'] = []
+requiredKeywords['QM'] = []
+requiredKeywords['RT'] = []
+
+for sec in knownKeywords:
+  for keyWord in knownKeywords[sec]:
+    if knownKeywords[sec][keyWord].req:
+      requiredKeywords[sec].append(keyWord)

src/python/src/parse/parseInput.py
@@ -42,33 +42,8 @@ def genSecDict(parser,section):
     'o-formu':parserFormU,
     'd3'     :parserDoubleArray
   }
-  
-# for opt in opts:
-#    dict1[opt] = parser.get(section,opt)
-#    try:
-#      readTyp = knownKeywords[section][opt.lower()].typ
-##      print opt
-#      if readTyp in ('i','d','s'):
-#        print 'reading I, D, or S: '+ repr(parseMap[readTyp])
-#      elif readTyp in ('o-orth','o-env','o-formu'):
-#        print 'reading something from RT: '+ repr(parseMap[readTyp])
-#        dict1[opt.lower()] = parseMap[readTyp](parser,section,opt)
-#      elif 'd' in readTyp and len(readTyp) > 1:
-#        print 'reading a double array of dim ' + readTyp[1:] + ": " + repr(parseMap[readTyp])
-#      else:
-#        print 'reading something else: '+str(opt)
-#     if readTyp in ('i','d','s'):
-#       dict1[opt.lower()] = parseMap[readTyp](section,opt)
-#     elif readTyp in ('o-orth','o-env','o-formu','d3'):
-#       dict1[opt.lower()] = parseMap[readTyp](parser,section,opt)
-#     else:
-#       print 'reading something else: '+str(opt)
-      
-#    except KeyError:
-#     print 'unrecognized keyword: '+str(opt)
-#    else:
-#      continue
 
+  keydict = ''
   try: 
     keydict = knownKeywords[section]
   except KeyError:
@@ -76,7 +51,6 @@ def genSecDict(parser,section):
     return
 
   for opt in opts:
-    keydict = ''
     keyword = ''
     readtyp = ''
 
@@ -88,12 +62,15 @@ def genSecDict(parser,section):
      
     readTyp = keyword.typ
 
-    if readTyp in ('i','s','d'):
+    if readTyp in ('i','d'):
       dict1[opt.lower()] = parseMap[readTyp](section,opt)
+    elif readTyp in ('s'):
+      dict1[opt.lower()] = parseMap[readTyp](section,opt).lower()
     elif readTyp in ('o-orth','o-env','o-formu','d3'):
       dict1[opt.lower()] = parseMap[readTyp](parser,section,opt)
     else:
       print 'Option data type not recognized'
+
   return dict1
 
 

src/python/src/parse/parseMolecule.py
@@ -2,6 +2,7 @@ import os,sys
 #sys.path.append('/home/dbwy/git_repo/chronusq/build_gcc_libint_openmp/src/python')
 #sys.path.append('/home/dbwy/git_repo/chronusq/src/python')
 import libpythonapi as chronusQ
+from meta.knownKeywords import requiredKeywords
 
 #
 #  Parse the Molecule section of the input file and populate
@@ -18,17 +19,15 @@ def parseMolecule(workers,settings):
   print 'Parsing Molecular Information'
 
 #
-# Check for unknown keywords in the Molecules section
+#  Check that all of the required keywords for Molecule
+#  object are found
 #
-# knownKeywords = [ 'charge', 'mult', 'geom' ]
-# for i in settings:
-#   if i not in knownKeywords:
-#     print "Keyword Molecule."+ str(i) +" not recognized"
+  for i in requiredKeywords['Molecule']:
+    if i not in settings:
+      print 'Required keyword Molecule.' + str(i) + ' not found'
+      exit(1)
 #
 # Grab charge and multiplicity
-#
-# FIXME: Need a check if these keywords aren't there
-#        or an ugly python error occurs
 #
   charge = settings['charge']
   mult   = settings['mult'  ]

commit 2c221a2e316c7e946c3a0b5967945654b51b1338

Fixed a bug introduced in a9c7cafa9a74057939 for RT

src/python/src/parse/parseQM.py
@@ -57,7 +57,7 @@ def parseQM(workers,secDict):
   except KeyError:
     print 'No Basis Set keyword found'
 
-  if ssSettings['job'] in ('RT'):
+  if ssSettings['job'] in ('rt'):
     parseRT(workers,secDict['rt']) 
 
 #  # Space filler to pasify error 