925a13bacb423596b0b3dc2755dbf37727c63f36
Joshua Goings <jjgoings@gmail.com>
2015-10-27 16:01:44 -0700

    one time intermediates finished

--- src/coupledcluster/coupledcluster.cpp
+++ src/coupledcluster/coupledcluster.cpp
@@ -59,7 +59,7 @@ double CoupledCluster::CCSD(){
   double Denom = 1.0;
 
   // One-time intermediates
-  RealTensor2d Zbc;
+  RealTensor2d Zbc, Tjk;
   RealTensor4d Sijkl, Yjkbc;
   // Intermediates
   RealTensor2d Hba, Hbj, Hij, Gca, Gik; 
@@ -75,6 +75,7 @@ double CoupledCluster::CCSD(){
     Sijkl   = RealTensor4d(this->nO_,this->nO_,this->nO_,this->nO_);
     Yjkbc   = RealTensor4d(this->nO_,this->nO_,this->nV_,this->nV_);
     Zbc     = RealTensor2d(this->nV_,this->nV_);
+    Tjk     = RealTensor2d(this->nO_,this->nO_);
     // Intermediates
     Hba     = RealTensor2d(this->nV_,this->nV_);
     Hbj     = RealTensor2d(this->nV_,this->nO_);
@@ -125,7 +126,57 @@ double CoupledCluster::CCSD(){
     for(auto j = 0; j < this->nO_; j++) 
     for(auto k = 0; k < this->nO_; k++) 
     for(auto l = 0; l < this->nO_; l++) { 
+      Sijkl(i,j,k,l) += (this->mointegrals_->IJKL(i,j,k,l))*(0.5);
+      for(auto c = 0; c < this->nV_; c++) {
+        Sijkl(i,j,k,l) -= (this->mointegrals_->IJKA(i,j,l,c))*Tia(k,c);
+        for(auto d = 0; d < this->nV_; d++) {
+          Sijkl(i,j,k,l) += (this->mointegrals_->IJAB(i,j,c,d))*Tau(k,l,c,d)*(0.25);
+        }
+      }
     }
+
+   // Zbc intermediate
+   for(auto b = 0; b < this->nV_; b++)
+   for(auto c = 0; c < this->nV_; c++)
+   for(auto k = 0; k < this->nO_; k++)
+   for(auto d = 0; d < this->nV_; d++) {
+     Zbc(b,c) -= (this->mointegrals_->IABC(k,c,d,b))*Tia(k,d);
+     for(auto l = 0; l < this->nO_; l++) {
+       Zbc(b,c) -= (this->mointegrals_->IJAB(k,l,d,b))*Tau(k,l,c,d)*(0.5);
+     }
+   }
+   // Tjk intermediate
+   for(auto j = 0; j < this->nO_; j++)
+   for(auto k = 0; k < this->nO_; k++)
+   for(auto l = 0; l < this->nO_; l++)
+   for(auto c = 0; c < this->nV_; c++) {
+     Tjk(j,k) -= (this->mointegrals_->IJKA)(l,j,k,c)*Tia(l,c);
+     for(auto d = 0; d < this->nV_; d++) {
+       Tjk(j,k) -= (this->mointegrals_->IJAB(l,j,c,d))*Tau(k,l,c,d)*(0.5);
+     }
+   }
+
+   // Yjkbc intermediate
+   for(auto j = 0; j < this->nO_; j++)
+   for(auto k = 0; k < this->nO_; k++)
+   for(auto b = 0; b < this->nV_; b++)
+   for(auto c = 0; c < this->nV_; c++) {
+      Yjkbc(j,k,b,c) -= this->mointegrals_->IAJB(j,c,k,b);
+     for(auto d = 0; d < this->nV_; d++) {
+       Yjkbc(j,k,b,c) -= (this->mointegrals_->IABC(j,c,d,b))*Tia(k,d);
+     }
+     for(auto l = 0; l < this->nO_; l++) {
+       Yjkbc(j,k,b,c) -= (this->mointegrals_->IJKA(l,j,k,b))*Tia(l,c);
+       for(auto d = 0; d < this->nV_; d++) {
+         Yjkbc(j,k,b,c) += (this->mointegrals_->IJAB(l,j,d,b))*
+                          (Tijab(k,l,c,d) - Tia(l,c)*Tia(k,d));
+       }
+     }
+   }
+
+   // Done with one time intermediates, begin iterations
+
+
   } 
   return ECorr;
 };
