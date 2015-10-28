89e21e5d997c58df26cf3adf7f54bb9341b797b8
Joshua Goings <jjgoings@gmail.com>
2015-10-27 17:37:44 -0700

    infrastructure there...but it still doesnt give correct values. need to debug

--- src/coupledcluster/coupledcluster.cpp
+++ src/coupledcluster/coupledcluster.cpp
@@ -55,8 +55,14 @@ void CoupledCluster::iniCoupledCluster( Molecule * molecule, BasisSet * basisSet
 
 double CoupledCluster::CCSD(){
   double ECorr = 0.0;
+  double ECCSD = 0.0;
   double EInit = 0.0;
   double Denom = 1.0;
+  double E1    = 0.0;
+  double E2    = 0.0;
+  double E3    = 0.0;
+  double E4    = 0.0;
+  double E5    = 0.0;
 
   // One-time intermediates
   RealTensor2d Zbc, Tjk;
@@ -66,9 +72,7 @@ double CoupledCluster::CCSD(){
   RealTensor4d Aijkl, Babcd, Hicak;
   // Amplitudes
   RealTensor2d Tia, Wia;
-  RealTensor4d Tijab, Tau, Wiajb;
-
-  auto NTCSxNBASIS = this->nTCS_*this->nBasis_;
+  RealTensor4d Tijab, Tau, Wijab;
 
   if(this->Ref_ == SingleSlater<double>::TCS){
     // Static (one-time) intermediates
@@ -90,7 +94,7 @@ double CoupledCluster::CCSD(){
     Wia     = RealTensor2d(this->nO_,this->nV_);
     Tijab   = RealTensor4d(this->nO_,this->nO_,this->nV_,this->nV_);
     Tau     = RealTensor4d(this->nO_,this->nO_,this->nV_,this->nV_);
-    Wiajb   = RealTensor4d(this->nO_,this->nV_,this->nO_,this->nV_);
+    Wijab   = RealTensor4d(this->nO_,this->nO_,this->nV_,this->nV_);
 
 
     // Initialize T1 and T2 amplitudes
@@ -175,8 +179,204 @@ double CoupledCluster::CCSD(){
    }
 
    // Done with one time intermediates, begin iterations
+   for(auto niter = 0; niter < 40; niter++) {
+     // Hab
+     for(auto b = 0; b < this->nV_; b++)
+     for(auto a = 0; a < this->nV_; a++)
+     for(auto j = 0; j < this->nO_; j++)
+     for(auto k = 0; k < this->nO_; k++)
+     for(auto c = 0; c < this->nV_; c++) {
+       Hba(b,a) -= (this->mointegrals_->IJAB(j,k,b,c))*Tau(j,k,a,c)*(0.5);
+     }
+
+    // Hij
+     for(auto i = 0; i < this->nO_; i++)
+     for(auto j = 0; j < this->nO_; j++)
+     for(auto k = 0; k < this->nO_; k++)
+     for(auto b = 0; b < this->nV_; b++)
+     for(auto c = 0; c < this->nV_; c++) {
+       Hij(i,j) += (this->mointegrals_->IJAB(j,k,b,c))*Tau(i,k,b,c)*(0.5);
+     }
+
+   // Hbj
+     for(auto b = 0; b < this->nV_; b++)
+     for(auto j = 0; j < this->nO_; j++)
+     for(auto k = 0; k < this->nO_; k++)
+     for(auto c = 0; c < this->nV_; c++) {
+       Hbj(b,j) += (this->mointegrals_->IJAB(j,k,b,c))*Tia(k,c);
+     }
+
+   // Gca
+     for(auto c = 0; c < this->nV_; c++) 
+     for(auto a = 0; a < this->nV_; a++) {
+       Gca(c,a) += Hba(c,a);
+       for(auto k = 0; k < this->nO_; k++) 
+       for(auto d = 0; d < this->nV_; d++) {
+         Gca(c,a) -= (this->mointegrals_->IABC(k,a,c,d))*Tia(k,d);
+       } 
+     }
 
 
-  } 
+   // Gik
+     for(auto i = 0; i < this->nO_; i++)
+     for(auto k = 0; k < this->nO_; k++) {
+       Gik(i,k) += Hij(i,k);
+       for(auto l = 0; l < this->nO_; l++)
+       for(auto c = 0; c < this->nV_; c++) {
+         Gik(i,k) += (this->mointegrals_->IJKA(k,l,i,c))*Tia(l,c);
+       }
+     }
+
+   // Aijkl
+     for(auto i = 0; i < this->nO_; i++)
+     for(auto j = 0; j < this->nO_; j++)
+     for(auto k = 0; k < this->nO_; k++)
+     for(auto l = 0; l < this->nO_; l++) {
+       Aijkl(i,j,k,l) += (this->mointegrals_->IJKL(i,j,k,l));
+       for(auto c = 0; c < this->nV_; c++) {
+         Aijkl(i,j,k,l) += (this->mointegrals_->IJKA(k,l,i,c))*Tia(j,c) 
+                          -(this->mointegrals_->IJKA(k,l,j,c))*Tia(i,c);
+         for(auto d = 0; d < this->nV_; d++) {
+           Aijkl(i,j,k,l) += (this->mointegrals_->IJAB(k,l,c,d))*Tau(i,j,c,d)*(0.5);
+         }
+       }
+     }
+
+   // Babcd
+     
+     for(auto c = 0; c < this->nV_; c++)
+     for(auto d = 0; d < this->nV_; d++)
+     for(auto a = 0; a < this->nV_; a++)
+     for(auto b = 0; b < this->nV_; b++) {
+       Babcd(a,b,c,d) += (this->mointegrals_->ABCD(a,b,c,d));
+       for(auto k = 0; k < this->nO_; k++) {
+         Babcd(a,b,c,d) += (this->mointegrals_->IABC(k,a,c,d))*Tia(k,b) 
+                          -(this->mointegrals_->IABC(k,b,c,d))*Tia(k,a);
+       }
+     }
+
+  // Hicak 
+     for(auto i = 0; i < this->nO_; i++)
+     for(auto c = 0; c < this->nV_; c++)
+     for(auto a = 0; a < this->nV_; a++)
+     for(auto k = 0; k < this->nO_; k++) {
+       Hicak(i,c,a,k) -= (this->mointegrals_->IAJB(i,c,k,a));
+       for(auto d = 0; d < this->nV_; d++) {
+         Hicak(i,c,a,k) -= (this->mointegrals_->IABC(k,a,d,c))*Tia(i,d);  
+       }
+       for(auto l = 0; l < this->nO_; l++)  
+       for(auto d = 0; d < this->nV_; d++) { 
+         Hicak(i,c,a,k) -= (this->mointegrals_->IJAB(k,l,c,d))*
+                           (Tijab(i,l,d,a)*(0.5) + Tia(i,d)*Tia(l,a));
+       }
+       for(auto l = 0; l < this->nO_; l++) { 
+         Hicak(i,c,a,k) -= (this->mointegrals_->IJKA(l,k,i,c))*Tia(l,a);
+       }
+     }
+
+  // Wia time!
+     for(auto i = 0; i < this->nO_; i++) 
+     for(auto a = 0; a < this->nV_; a++) 
+     for(auto j = 0; j < this->nO_; j++) {
+       Wia(i,a) -= Hij(i,j)*Tia(j,a);
+       for(auto b = 0; b < this->nV_; b++)  
+       for(auto c = 0; c < this->nV_; c++) { 
+         Wia(i,a) -= (this->mointegrals_->IABC(j,a,b,c))*Tau(i,j,b,c)*(0.5);
+       }
+     }
+     for(auto i = 0; i < this->nO_; i++) 
+     for(auto a = 0; a < this->nV_; a++) { 
+       for(auto b = 0; b < this->nV_; b++) {
+         Wia(i,a) += Hba(b,a)*Tia(i,b);
+       }
+       for(auto j = 0; j < this->nO_; j++)
+       for(auto b = 0; b < this->nV_; b++) {
+         Wia(i,a) += Hbj(b,j)*(Tijab(i,j,a,b) + Tia(i,b)*Tia(j,a))
+                    -(this->mointegrals_->IAJB(i,b,j,a)*Tia(j,b));
+         for(auto k = 0; k < this->nO_; k++) {
+           Wia(i,a) -= (this->mointegrals_->IJKA(j,k,i,b))*Tau(j,k,a,b)*(0.5);
+         }
+       }
+     }
+     
+   // Wijab
+     for(auto i = 0; i < this->nO_; i++) 
+     for(auto j = 0; j < this->nO_; j++) 
+     for(auto a = 0; a < this->nV_; a++) 
+     for(auto b = 0; b < this->nV_; b++) { 
+       for(auto k = 0; k < this->nO_; k++)  
+       for(auto l = 0; l < this->nO_; l++) {
+       Wijab(i,j,a,b) += Aijkl(i,j,k,l)*Tau(k,l,a,b)*(0.5);
+       }
+       E1 += Wijab(i,j,a,b)*Tijab(i,j,a,b);
+       for(auto c = 0; c < this->nV_; c++)  
+       for(auto d = 0; d < this->nV_; d++) {
+         Wijab(i,j,a,b) += Babcd(a,b,c,d)*Tau(i,j,c,d)*(0.5);
+       }
+       E2 += Wijab(i,j,a,b)*Tijab(i,j,a,b);
+       for(auto c = 0; c < this->nV_; c++) {
+         Wijab(i,j,a,b) += Gca(c,a)*Tijab(i,j,c,b) -
+                           Gca(c,b)*Tijab(i,j,c,b) -
+                           (this->mointegrals_->IABC(j,c,a,b))*Tia(i,c) +
+                           (this->mointegrals_->IABC(i,c,a,b))*Tia(j,c); 
+       }
+       E3 += Wijab(i,j,a,b)*Tijab(i,j,a,b);
+       for(auto k = 0; k < this->nO_; k++) {
+         Wijab(i,j,a,b) += (this->mointegrals_->IJKA(i,j,k,a))*Tia(k,b) -
+                           (this->mointegrals_->IJKA(i,j,k,b))*Tia(k,a) -
+                           Gik(i,k)*Tijab(k,j,a,b) +
+                           Gik(j,k)*Tijab(k,i,a,b); 
+       }
+       E4 += Wijab(i,j,a,b)*Tijab(i,j,a,b);
+       for(auto k = 0; k < this->nO_; k++) 
+       for(auto c = 0; c < this->nV_; c++) {
+         Wijab(i,j,a,b) += Hicak(i,c,a,k)*Tijab(j,k,b,c) -
+                           Hicak(j,c,a,k)*Tijab(i,k,b,c) -
+                           Hicak(i,c,b,k)*Tijab(j,k,a,c) +
+                           Hicak(j,c,b,k)*Tijab(i,k,a,c) +
+                           (this->mointegrals_->IAJB(i,c,k,a))*Tia(j,c)*Tia(k,b) -
+                           (this->mointegrals_->IAJB(j,c,k,a))*Tia(i,c)*Tia(k,b) -
+                           (this->mointegrals_->IAJB(i,c,k,b))*Tia(j,c)*Tia(k,a) +
+                           (this->mointegrals_->IAJB(j,c,k,b))*Tia(i,c)*Tia(k,a); 
+       }
+       E5 += Wijab(i,j,a,b)*Tijab(i,j,a,b);
+     } // end ijab 
+ 
+    // Make new T amplitudes
+     for(auto i = 0; i < this->nO_; i++)
+     for(auto a = 0; i < this->nO_; i++) {
+       Denom = (*this->singleSlater_->epsA())(i)
+               - (*this->singleSlater_->epsA())(a + this->nO_);
+       Tia(i,a) = Wia(i,a)/Denom;
+     }
+     for(auto i = 0; i < this->nO_; i++) 
+     for(auto j = 0; j < this->nO_; j++) 
+     for(auto a = 0; a < this->nV_; a++) 
+     for(auto b = 0; b < this->nV_; b++) { 
+       Denom = (*this->singleSlater_->epsA())(i)
+               + (*this->singleSlater_->epsA())(j)
+               - (*this->singleSlater_->epsA())(a + this->nO_)
+               - (*this->singleSlater_->epsA())(b + this->nO_);
+       Tijab(i,j,a,b) = (Wijab(i,j,a,b) + (this->mointegrals_->IJAB(i,j,a,b)))/Denom; 
+     }
+
+     for(auto i = 0; i < this->nO_; i++) 
+     for(auto j = 0; j < this->nO_; j++) 
+     for(auto a = 0; a < this->nV_; a++) 
+     for(auto b = 0; b < this->nV_; b++) { 
+       Tau(i,j,a,b) = Tijab(i,j,a,b) + (Tia(i,a)*Tia(j,b) -
+                                        Tia(i,b)*Tia(j,a));
+       ECCSD += (this->mointegrals_->IJAB(i,j,a,b))*Tau(i,j,a,b);
+     }
+    
+     ECCSD = ECCSD*(0.25);
+     cout << "iter # " << niter << "   " << ECCSD << endl;
+    
+
+
+
+    } // end CCSD iteration
+  } // end if TCS
+  ECorr = ECCSD;
   return ECorr;
 };
