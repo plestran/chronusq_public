8d6594c0559bd06c0fb0a22cda31be16c4f88134
David Williams-Young <dbwy@uw.edu>
2016-03-09 12:50:38 -0500

    Added warning supression by default for unimportant warnings, moved boost headers to supressed preprocesser redefined warnings

--- include/global.h
+++ include/global.h
@@ -34,6 +34,15 @@
 // CMake Compilation Configuration
 #include <config_chronusq.h>
 
+// Boost Headers
+#include <boost/python.hpp>
+#include <boost/geometry/geometry.hpp>
+#include <boost/math/constants/constants.hpp>
+#include <boost/math/special_functions/gamma.hpp>
+#include <boost/math/special_functions/asinh.hpp>
+#include <boost/algorithm/string.hpp>
+
+
 // IO
 #include <iostream>
 #include <fstream>
@@ -67,13 +76,6 @@
 //#include "oompi.h"
 //#include <pthread.h>
 
-// Boost Headers
-#include <boost/geometry/geometry.hpp>
-#include <boost/math/constants/constants.hpp>
-#include <boost/python.hpp>
-#include <boost/math/special_functions/gamma.hpp>
-#include <boost/math/special_functions/asinh.hpp>
-#include <boost/algorithm/string.hpp>
 
 // Misc
 #include <stdlib.h>
