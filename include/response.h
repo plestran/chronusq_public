a39ab6eaf1c32dc3d09cd22eee3884fa0e500e52
David Williams-Young <dbwy@uw.edu>
2016-03-25 15:32:37 -0400

    Added PostSCF class to hold PostSCF Dims

--- include/response.h
+++ include/response.h
@@ -31,6 +31,7 @@
 #include <qn.h>
 #include <singleslater.h>
 #include <mointegrals.h>
+#include <postscf.h>
 
 
 
@@ -90,7 +91,7 @@ enum RESPONSE_JOB_TYPE {
   DYNAMIC
 };
 template<typename T>
-class Response : public QNCallable<T>, public Quantum<T> {
+class Response : public QNCallable<T>, public Quantum<T>, public PostSCF {
 
   /** Useful TypeDefs **/
   typedef Eigen::Matrix<T,Dynamic,Dynamic,ColMajor> TMat;
@@ -136,48 +137,6 @@ class Response : public QNCallable<T>, public Quantum<T> {
   bool doMixedAB_;       ///< (?) Do Mixed Alpha-Beta Block ** PPRPA ONLY **
   bool doAllBeta_;       ///< (?) Do All Beta Block ** PPRPA ONLY **
   
-
-  /** Derived Dimensions Post-SCF **/
-  // Single Dimensions
-  int       nOA_; ///< Number of occupied orbitals (Alpha)
-  int       nVA_; ///< Number of virtual orbitals (Alpha)
-  int       nOB_; ///< Number of occupied orbitals (Beta)
-  int       nVB_; ///< Number of virtual orbitals (Beta)
-  int       nO_;  ///< Total number of occupied orbitals NOA + NOB
-  int       nV_;  ///< Total number of virtual orbitals NVA + NVB
-
-  // Quadratic Dimenstions
-    
-  // Occupied-Occupied
-  int       nOAOA_;      ///< NOA * NOA
-  int       nOBOB_;      ///< NOB * NOB
-  int       nOAOB_;      ///< NOA * NOB
-  int       nOO_;        ///< NO * NO
-  int       nOAOA_SLT_;  ///< NOA * (NOA - 1) / 2
-  int       nOBOB_SLT_;  ///< NOB * (NOB - 1) / 2
-  int       nOO_SLT_;    ///< NO * (NO - 1) / 2
-  int       nOAOA_LT_;   ///< NOA * (NOA + 1) / 2
-  int       nOO_LT_;     ///< NO * (NO + 1) / 2
-
-  // Occupied-Virtual
-  int       nOAVA_;      ///< NOA * NVA
-  int       nOBVB_;      ///< NOB * NVB
-  int       nOAVB_;      ///< NOA * NVB
-  int       nOBVA_;      ///< NOB * NVA
-  int       nOV_;        ///< NO * NV
-
-  // Virtual-Virtual
-  int       nVAVA_;      ///< NVA * NVA
-  int       nVBVB_;      ///< NVB * NVB
-  int       nVAVB_;      ///< NVA * NVB
-  int       nVV_;        ///< NV * NV
-  int       nVAVA_SLT_;  ///< NVA * (NVA - 1) / 2
-  int       nVBVB_SLT_;  ///< NVB * (NVB - 1) / 2
-  int       nVV_SLT_;    ///< NV * (NV - 1) / 2
-  int       nVAVA_LT_;   ///< NVA * (NVA + 1) / 2
-  int       nVV_LT_;     ///< NV * (NV + 1) / 2
-
-
   /** Misc values **/
   double rMu_; ///< Level shift, currently only used for PP-methods
 
@@ -200,7 +159,7 @@ public:
    *  Default Constructor loads default values
    *  Inherits QNCallable default constructor
    */ 
-  Response() : QNCallable<T>(), Quantum<T>(){
+  Response() : QNCallable<T>(), Quantum<T>(), PostSCF(){
     // Intialize pointers to NULL
     this->fileio_       = NULL;
     this->mointegrals_  = NULL;
@@ -210,37 +169,6 @@ public:
     this->nBasis_     = 0;
 //  this->nTCS_       = 0;
     this->Ref_        = 0;
-
-    // Zero out PSCF dimensions to be built later
-    this->nOA_        = 0;
-    this->nVA_        = 0;
-    this->nOB_        = 0;
-    this->nVB_        = 0;
-    this->nOAVA_      = 0;      
-    this->nOBVB_      = 0;      
-    this->nOAVB_      = 0;      
-    this->nOBVA_      = 0;      
-    this->nVAVA_SLT_  = 0;  
-    this->nVBVB_SLT_  = 0;  
-    this->nVAVA_LT_   = 0;   
-    this->nVAVA_      = 0;      
-    this->nVBVB_      = 0;      
-    this->nOAOA_SLT_  = 0;  
-    this->nOBOB_SLT_  = 0;  
-    this->nOAOA_LT_   = 0;   
-    this->nOAOA_      = 0;      
-    this->nOBOB_      = 0;      
-    this->nVAVB_      = 0;      
-    this->nOAOB_      = 0;      
-    this->nO_         = 0;         
-    this->nV_         = 0;         
-    this->nOV_        = 0;        
-    this->nVV_SLT_    = 0;    
-    this->nVV_LT_     = 0;     
-    this->nVV_        = 0;        
-    this->nOO_SLT_    = 0;    
-    this->nOO_LT_     = 0;     
-    this->nOO_        = 0;        
 
     // Zero out Misc values
     this->rMu_ = 0.0;
