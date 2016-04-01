90cd961fc8d9abc3213a62a036d096366a6522f9
David Williams-Young <dbwy@uw.edu>
2016-04-01 19:02:20 -0400

    implemented new (still wrong) 2PDM for ppTDA

--- src/playground/playground.cxx
+++ src/playground/playground.cxx
@@ -98,69 +98,82 @@ int main(int argc, char **argv){
     else       return 0;
   };
 
-  // This builds the total difference density in the AO basis
-  for(auto mu = 0; mu < singleSlater.nBasis(); mu++)
-  for(auto nu = 0; nu < singleSlater.nBasis(); nu++)
-  for(auto a = 0, ab = 0; a < resp.nVA(); a++      )
-  for(auto b = 0        ; b < resp.nVB(); b++, ab++)
-  for(auto c = 0, cd = 0; c < resp.nVA(); c++      )
-  for(auto d = 0        ; d < resp.nVB(); d++, cd++){
-    TAO2(mu,nu) += T.col(5)(ab) * T.col(5)(cd) * ( 
-       (*singleSlater.moA())(mu,resp.nOA() + a) * 
-       (*singleSlater.moA())(nu,resp.nOA() + c) * delta(b,d)
-      + 
-       (*singleSlater.moA())(mu,resp.nOA() + b) * 
-       (*singleSlater.moA())(nu,resp.nOA() + d) * delta(a,c)
-    ); 
-  };
+  for(auto iSt = 0; iSt < 8 ; iSt++){
+     // This builds the total difference density in the AO basis
+     for(auto mu = 0; mu < singleSlater.nBasis(); mu++)
+     for(auto nu = 0; nu < singleSlater.nBasis(); nu++)
+     for(auto a = 0, ab = 0; a < resp.nVA(); a++      )
+     for(auto b = 0        ; b < resp.nVB(); b++, ab++)
+     for(auto c = 0, cd = 0; c < resp.nVA(); c++      )
+     for(auto d = 0        ; d < resp.nVB(); d++, cd++){
+       TAO2(mu,nu) += T.col(iSt)(ab) * T.col(iSt)(cd) * ( 
+          (*singleSlater.moA())(mu,resp.nOA() + a) * 
+          (*singleSlater.moA())(nu,resp.nOA() + c) * delta(b,d)
+         + 
+          (*singleSlater.moA())(mu,resp.nOA() + b) * 
+          (*singleSlater.moA())(nu,resp.nOA() + d) * delta(a,c)
+       ); 
+     };
+   
+     // This builds the (alpha) difference density in the MO basis
+     for(auto a = 0; a < resp.nVA(); a++      )
+     for(auto b = 0        ; b < resp.nVB(); b++)
+     for(auto c = 0, cd = 0; c < resp.nVA(); c++      )
+     for(auto d = 0        ; d < resp.nVB(); d++, cd++)
+     for(auto e = 0, ef = 0; e < resp.nVA(); e++      )
+     for(auto f = 0        ; f < resp.nVB(); f++, ef++){
+       TMO(resp.nOA() + a, resp.nOB() + b) += 
+         T.col(iSt)(cd) * T.col(iSt)(ef) *
+         delta(b,f) * delta(c,e) * delta(d,a);
+     }
+   
+     // This transforms the (alpha) difference density into the AO basis
+     TAO = (*singleSlater.moA()) * TMO * singleSlater.moA()->adjoint();
 
-  // This builds the (alpha) difference density in the MO basis
-  for(auto a = 0; a < resp.nVA(); a++      )
-  for(auto b = 0        ; b < resp.nVB(); b++)
-  for(auto c = 0, cd = 0; c < resp.nVA(); c++      )
-  for(auto d = 0        ; d < resp.nVB(); d++, cd++)
-  for(auto e = 0, ef = 0; e < resp.nVA(); e++      )
-  for(auto f = 0        ; f < resp.nVB(); f++, ef++){
-    TMO(resp.nOA() + a, resp.nOB() + b) += 
-      T.col(5)(cd) * T.col(5)(ef) *
-      delta(b,f) * delta(c,e) * delta(d,a);
+     cout <<TAO2.cwiseQuotient(TAO) << endl;
+   
+   //prettyPrint(cout,TMO,"TMO");
+   //prettyPrint(cout,TAO,"TAO");
+   //prettyPrint(cout,TAO2,"TAO2");
+   //cout << T.col(iSt).dot(T.col(iSt)) << endl;
+   //cout << (TAO * (*aoints.overlap_)).trace() << endl;
+   //cout << (TAO2 * (*aoints.overlap_)).trace() << endl;
+   
+     RealMatrix XAO(singleSlater.nBasis(),singleSlater.nBasis());
+     for(auto mu = 0; mu < singleSlater.nBasis(); mu++)
+     for(auto nu = 0; nu < singleSlater.nBasis(); nu++)
+     for(auto a = 0, ab = 0; a < resp.nVA(); a++      )
+     for(auto b = 0        ; b < resp.nVB(); b++, ab++){
+       XAO(mu,nu) += T.col(iSt)(ab) * 
+         (*singleSlater.moA())(mu,resp.nOA() + a) *  
+         (*singleSlater.moA())(nu,resp.nOA() + b); 
+     }
+   
+   //prettyPrint(cout,T.col(iSt),"X");
+   //prettyPrint(cout,XAO,"XAO");
+     /*
+     double TPDM = (TAO2 * (*aoints.overlap_)).trace() *
+     0.25*((*singleSlater.densityA()) * (*aoints.overlap_)).trace()
+     + 
+     0.25*(TAO2 * (*aoints.overlap_) * (*singleSlater.densityA()) * (*aoints.overlap_))
+        .trace() 
+     +
+     0.0*(XAO * (*aoints.overlap_) * XAO.adjoint() * (*aoints.overlap_)).trace();
+     */
+   
+    /*
+     double TPDM = -2.0* (TAO * (*aoints.overlap_) * (*singleSlater.densityA()) * (*aoints.overlap_)).trace() 
+       + 2.0 * (XAO * (*aoints.overlap_) * XAO.adjoint() * (*aoints.overlap_)).trace();
+       */
+     double TPDM = -0.5* (TAO2 * (*aoints.overlap_) * (*singleSlater.densityA()) * (*aoints.overlap_)).trace() 
+       + 0.0 * (XAO * (*aoints.overlap_) * XAO.adjoint() * (*aoints.overlap_)).trace();
+   
+     double OPDM = 2;
+   
+     cout << (3.0*OPDM + TPDM)/4 << endl;;
+   //cout << OPDM << endl;;
+   //cout << TPDM << endl;;
   }
-
-  // This transforms the (alpha) difference density into the AO basis
-  TAO = (*singleSlater.moA()) * TMO * singleSlater.moA()->adjoint();
-
-//prettyPrint(cout,TMO,"TMO");
-//prettyPrint(cout,TAO,"TAO");
-//prettyPrint(cout,TAO2,"TAO2");
-  cout << T.col(5).dot(T.col(5)) << endl;
-  cout << (TAO * (*aoints.overlap_)).trace() << endl;
-  cout << (TAO2 * (*aoints.overlap_)).trace() << endl;
-
-  RealMatrix XAO(singleSlater.nBasis(),singleSlater.nBasis());
-  for(auto mu = 0; mu < singleSlater.nBasis(); mu++)
-  for(auto nu = 0; nu < singleSlater.nBasis(); nu++)
-  for(auto a = 0, ab = 0; a < resp.nVA(); a++      )
-  for(auto b = 0        ; b < resp.nVB(); b++, ab++){
-    XAO(mu,nu) += T.col(5)(ab) * 
-      (*singleSlater.moA())(mu,resp.nOA() + a) *  
-      (*singleSlater.moA())(nu,resp.nOA() + b); 
-  }
-
-  prettyPrint(cout,T.col(5),"X");
-  prettyPrint(cout,XAO,"XAO");
-  double TPDM = (TAO2 * (*aoints.overlap_)).trace() *
-  0.25*((*singleSlater.densityA()) * (*aoints.overlap_)).trace()
-  + 
-  0.25*(TAO2 * (*aoints.overlap_) * (*singleSlater.densityA()) * (*aoints.overlap_))
-     .trace() 
-  +
-  0.0*(XAO * (*aoints.overlap_) * XAO.adjoint() * (*aoints.overlap_)).trace();
-
-  double OPDM = 2;
-
-  cout << 3.0*OPDM - 1.5*TPDM << endl;;
-  cout << OPDM << endl;;
-  cout << TPDM << endl;;
 
    
   finalizeCQ(); 
