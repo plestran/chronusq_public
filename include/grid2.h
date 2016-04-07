ef45d45961cae76c142ae57f3fe6bd52323d2572
David Williams-Young <dbwy@uw.edu>
2016-04-07 18:25:51 -0400

    Added documentation on Gauss-Chebyshev grid, implements GC of 2nd kind

--- include/grid2.h
+++ include/grid2.h
@@ -95,15 +95,45 @@ class OneDGrid2 : public Grid2 {
     virtual IntegrationPoint operator[](size_t) = 0;
 };
 
+/**
+ * Gauss-Chebyshev Quadrature of the first kind
+ *
+ * \int _{-1}^{1} \frac{f(x)}{\sqrt{1 - x^2}} \mathrm{d} x = \sum_n w_i f(x_i)
+ *
+ * x_i = \cos\left( \frac{2i - 1}{2n} \pi \right)
+ * w_i = \frac{\pi}{n}
+ *
+ * where "n" is the number of integration points
+ *
+ */
 class GaussChebFst : public OneDGrid2 {
   public:
     GaussChebFst(size_t npts = 0) : OneDGrid2(npts){ };
     ~GaussChebFst(){ };
     IntegrationPoint operator[](size_t i){ 
+      // Generate points and weights for (-1,1)
       double pt  = std::cos( (2.0*(i+1)-1.0) / (2*this->nPts_) * math.pi );
       double wgt = (math.pi / this->nPts_);
 
+      // Map (-1,1) -> (0,\inf)
+        
+      // As we're integrating f(x) not \frac{f(x)}{\sqrt{1 - x^2}}
+      // we must factor the \sqrt{1-x^2} into the weights
+      //
+      // Also, we must factor in the Jacobian of the domain shift into
+      // the weights 
+      //
+      // dr = \frac{2}{(1-x)^2} dx
+      //
+      // Both of these factors lead to
+      //
+      // w_i \rightarrow w_i(x_i) = \frac{2\sqrt{1-x^2}}{(1-x)^2}
+      //
       wgt *= 2.0 * std::sqrt(1 - pt * pt) / ( (1 - pt) * (1 - pt) );
+
+      // Perform the coordinate shift
+      //
+      // r_i = \frac{(1+x_i)}{(1-x_i)}
       pt   = (1 + pt) / (1 - pt);
 
       return IntegrationPoint(pt,wgt);
@@ -111,11 +141,54 @@ class GaussChebFst : public OneDGrid2 {
     };
 };
 
+/**
+ * Gauss-Chebyshev Quadrature of the second kind
+ *
+ * \int _{-1}^{1} f(x)\sqrt{1 - x^2} \mathrm{d} x = \sum_n w_i f(x_i)
+ *
+ * x_i = \cos\left( \frac{i}{n + 1} \pi \right)
+ * w_i = \frac{\pi}{n+1} \sin^2\left( \frac{i}{n+1} \pi \right)
+ *
+ * where "n" is the number of integration points
+ *
+ */
 class GaussChebSnd : public OneDGrid2 {
   public:
-    GaussChebSnd(size_t npts) : OneDGrid2(npts){ };
+    GaussChebSnd(size_t npts = 0) : OneDGrid2(npts){ };
     ~GaussChebSnd(){ };
-    IntegrationPoint operator[](size_t i){ return IntegrationPoint(0,0);};
+    IntegrationPoint operator[](size_t i){ 
+      // Generate points and weights for (-1,1)
+      double pt = std::cos(math.pi * i / (this->nPts_ +1));
+      double sgt = math.pi / (this->nPts_ + 1) * 
+        std::pow(
+            std::sin(math.pi * i / (this->nPts_ +1)),
+            2.0
+        );
+
+      // Map (-1,1) -> (0,\inf)
+        
+      // As we're integrating f(x) not f(x)\sqrt{1 - x^2}
+      // we must factor the \frac{1}{\sqrt{1-x^2}} into the weights
+      //
+      // Also, we must factor in the Jacobian of the domain shift into
+      // the weights 
+      //
+      // dr = \frac{2}{(1-x)^2} dx
+      //
+      // Both of these factors lead to
+      //
+      // w_i \rightarrow w_i(x_i) = \frac{2\sqrt{1-x^2}}{(1-x)^2}
+      //
+      wgt *= 2.0 / (std::sqrt(1 - pt * pt) * (1 - pt) * (1 - pt) );
+
+
+      // Perform the coordinate shift
+      //
+      // r_i = \frac{(1+x_i)}{(1-x_i)}
+      pt   = (1 + pt) / (1 - pt);
+
+      return IntegrationPoint(pt,wgt);
+    };
 };
 
 class EulerMac : public OneDGrid2 {
