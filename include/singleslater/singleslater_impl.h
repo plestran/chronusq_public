5de878e1ef4052c3beefad1f9e2d9518e2d653b6
David Williams-Young <dbwy@uw.edu>
2016-08-04 10:57:11 -0700

    Added an IMPL file to explicitly instantiate the templated SS classes

---
+++ include/singleslater/singleslater_impl.h
@@ -0,0 +1,13 @@
+#include <singleslater.h>
+namespace ChronusQ{
+#include <singleslater/singleslater_alloc.h>
+#include <singleslater/singleslater_guess.h>
+#include <singleslater/singleslater_print.h>
+#include <singleslater/singleslater_fock.h>
+#include <singleslater/singleslater_misc.h>
+#include <singleslater/singleslater_scf.h>
+#include <singleslater/singleslater_diis.h>
+#include <singleslater/singleslater_properties.h>
+//#include <singleslater_dft.h>
+};
+
