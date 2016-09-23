63d7ff53c97fca3e24c224cdfc128a07869bac46
David Williams-Young <dbwy@uw.edu>
2016-09-23 10:55:58 -0700

    Added AOIntegrals Ortho functions to IMPL file

--- include/aointegrals/aointegrals_impl.h
+++ include/aointegrals/aointegrals_impl.h
@@ -3,6 +3,7 @@
 #include <aointegrals.h>
 namespace ChronusQ {
 #include <aointegrals/aointegrals_contract.h>
+#include <aointegrals/aointegrals_ortho.h>
 };
 #endif
 
--- src/aointegrals/aointegrals_impl.cpp
+++ src/aointegrals/aointegrals_impl.cpp
@@ -40,3 +40,20 @@ template void ChronusQ::AOIntegrals::newTwoEContractIncore(
       std::vector<std::reference_wrapper<ComplexMap>>&,
       std::vector<ERI_CONTRACTION_TYPE>&,
       std::vector<double>&) ;
+
+template void ChronusQ::AOIntegrals::Ortho1Trans(RealMatrix&,RealMatrix&);
+template void ChronusQ::AOIntegrals::Ortho1Trans(ComplexMatrix&,ComplexMatrix&);
+template void ChronusQ::AOIntegrals::Ortho1Trans(RealMap&,RealMap&);
+template void ChronusQ::AOIntegrals::Ortho1Trans(ComplexMap&,ComplexMap&);
+template void ChronusQ::AOIntegrals::Ortho1TransT(RealMatrix&,RealMatrix&);
+template void ChronusQ::AOIntegrals::Ortho1TransT(ComplexMatrix&,ComplexMatrix&);
+template void ChronusQ::AOIntegrals::Ortho1TransT(RealMap&,RealMap&);
+template void ChronusQ::AOIntegrals::Ortho1TransT(ComplexMap&,ComplexMap&);
+template void ChronusQ::AOIntegrals::Ortho2Trans(RealMatrix&,RealMatrix&);
+template void ChronusQ::AOIntegrals::Ortho2Trans(ComplexMatrix&,ComplexMatrix&);
+template void ChronusQ::AOIntegrals::Ortho2Trans(RealMap&,RealMap&);
+template void ChronusQ::AOIntegrals::Ortho2Trans(ComplexMap&,ComplexMap&);
+//template void ChronusQ::AOIntegrals::Ortho2TransT(RealMatrix&,RealMatrix&);
+//template void ChronusQ::AOIntegrals::Ortho2TransT(ComplexMatrix&,ComplexMatrix&);
+//template void ChronusQ::AOIntegrals::Ortho2TransT(RealMap&,RealMap&);
+//template void ChronusQ::AOIntegrals::Ortho2TransT(ComplexMap&,ComplexMap&);
