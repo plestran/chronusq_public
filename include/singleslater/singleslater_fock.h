981a8bb5d466650b7c36ccdd47372e2dc29fb63f
David Williams-Young <dbwy@uw.edu>
2016-11-02 08:54:59 -0700

    Sorted out almost all of the bits for parallel DFT

--- include/singleslater/singleslater_fock.h
+++ include/singleslater/singleslater_fock.h
@@ -267,6 +267,8 @@ void SingleSlater<T>::formVXC_new(){
 //Timing
 //  this->screenVxc = false;
 
+  int nthreads = omp_get_max_threads();
+
   std::chrono::high_resolution_clock::time_point start;
   std::chrono::high_resolution_clock::time_point finish;
   std::chrono::duration<double> duration_formVxc;
@@ -286,17 +288,43 @@ void SingleSlater<T>::formVXC_new(){
 */
 //  bool isGGA   = true;
 //  bool isGGA   = false;
+/*
   RealMatrix SCRATCH2(this->nBasis_,this->nBasis_);
-  VectorXd   SCRATCH1(this->nBasis_);
   RealMatrix SCRATCH2X(this->nBasis_,this->nBasis_);
   RealMatrix SCRATCH2Y(this->nBasis_,this->nBasis_);
   RealMatrix SCRATCH2Z(this->nBasis_,this->nBasis_);
+  VectorXd   SCRATCH1(this->nBasis_);
   VectorXd   SCRATCH1X(this->nBasis_);
   VectorXd   SCRATCH1Y(this->nBasis_);
   VectorXd   SCRATCH1Z(this->nBasis_);
+*/
+  
+  std::vector<RealVecMap> SCRATCH1;
+  std::vector<RealVecMap> SCRATCH1X;
+  std::vector<RealVecMap> SCRATCH1Y;
+  std::vector<RealVecMap> SCRATCH1Z;
+
+
   int NDer = 0;
   if(this->isGGA) NDer = 1; 
+  for(auto ithread = 0; ithread < nthreads; ithread++){
+    SCRATCH1.emplace_back(
+      this->memManager_->template malloc<double>(this->nBasis_),
+      this->nBasis_);
+    if(this->isGGA) {
+      SCRATCH1X.emplace_back(
+        this->memManager_->template malloc<double>(this->nBasis_),
+        this->nBasis_);
+      SCRATCH1Y.emplace_back(
+        this->memManager_->template malloc<double>(this->nBasis_),
+        this->nBasis_);
+      SCRATCH1Z.emplace_back(
+        this->memManager_->template malloc<double>(this->nBasis_),
+        this->nBasis_);
+    }
+  }
 
+/*
   std::array<double,3>  drhoT = {0.0,0.0,0.0}; ///< array TOTAL density gradient components
   std::array<double,3>  drhoS = {0.0,0.0,0.0}; ///< array SPIN  density gradient components
   std::array<double,3>  drhoA = {0.0,0.0,0.0}; ///< array ALPHA  density gradient components
@@ -311,6 +339,30 @@ void SingleSlater<T>::formVXC_new(){
   double gammaAA = 0.0;
   double gammaBB = 0.0;
   double gammaAB = 0.0;
+*/
+
+  std::vector<std::array<double,3>>  drhoT(nthreads,{0.0,0.0,0.0}); ///< array TOTAL density gradient components
+  std::vector<std::array<double,3>>  drhoS(nthreads,{0.0,0.0,0.0}); ///< array SPIN  density gradient components
+  std::vector<std::array<double,3>>  drhoA(nthreads,{0.0,0.0,0.0}); ///< array ALPHA  density gradient components
+  std::vector<std::array<double,3>>  drhoB(nthreads,{0.0,0.0,0.0}); ///< array BETA  density gradient components
+  std::vector<RealVecMap> GradRhoT;
+  std::vector<RealVecMap> GradRhoS;
+  std::vector<RealVecMap> GradRhoA;
+  std::vector<RealVecMap> GradRhoB;
+  for(auto ithread = 0; ithread < nthreads; ithread++){
+    GradRhoT.emplace_back(&drhoT[ithread][0],3);
+    GradRhoS.emplace_back(&drhoS[ithread][0],3);
+    GradRhoA.emplace_back(&drhoA[ithread][0],3);
+    GradRhoB.emplace_back(&drhoB[ithread][0],3);
+  }
+
+//std::vector<double> rhoA(nthreads,0.);
+//std::vector<double> rhoB(nthreads,0.);
+//std::vector<double> gammaAA(nthreads,0.);
+//std::vector<double> gammaBB(nthreads,0.);
+//std::vector<double> gammaAB(nthreads,0.);
+
+
   std::vector<bool> shMap(this->basisset_->nShell()+1);
   int NSkip2(0);
   int NSkip3(0);
@@ -321,35 +373,46 @@ void SingleSlater<T>::formVXC_new(){
   auto Newstart = std::chrono::high_resolution_clock::now();
   auto Newend = std::chrono::high_resolution_clock::now();
 
-  std::vector<std::size_t> closeShells;
-  VectorXd OmegaA(this->nBasis_), OmegaB(this->nBasis_);
+
+//VectorXd OmegaA(this->nBasis_), OmegaB(this->nBasis_);
+  std::vector<RealVecMap> OmegaA, OmegaB;
+  for(auto ithread = 0; ithread < nthreads; ithread++){
+    OmegaA.emplace_back(
+      this->memManager_->template malloc<double>(this->nBasis_),
+      this->nBasis_);
+    OmegaB.emplace_back(
+      this->memManager_->template malloc<double>(this->nBasis_),
+      this->nBasis_);
+  }
+
 //  VectorXd DENCOL(this->nBasis_);
   double fact = 2.0;
-  double *SCRATCH1DATA = SCRATCH1.data();
-  double *SCRATCH1XDATA = SCRATCH1X.data();
-  double *SCRATCH1YDATA = SCRATCH1Y.data();
-  double *SCRATCH1ZDATA = SCRATCH1Z.data();
-  double *OmegaADATA = OmegaA.data();
-  double *OmegaBDATA = OmegaB.data();
 
   std::vector<std::size_t> shSizes;
   for(auto iSh = 0; iSh < this->basisset_->nShell(); iSh++)
     shSizes.push_back(this->basisset_->shells(iSh).size());
 
+  std::vector<std::vector<std::size_t>> closeShells;
 // If screen in not on loop over all shell for each atoms, no need to
 // figure which are the close shell to each point
   if (!this->screenVxc){
-    closeShells.resize(this->basisset_->nShell());
-    std::iota(closeShells.begin(),closeShells.end(),0);
-    cout << "MAch. Prec " << std::numeric_limits<double>::epsilon() << endl;
-    for(auto iShell : closeShells) {
-       cout << "iShell " << iShell << " closeS[] " << closeShells[iShell] << endl; 
+    for(auto ithread = 0; ithread < nthreads; ithread++){
+      closeShells[ithread].resize(this->basisset_->nShell());
+      std::iota(closeShells[ithread].begin(),closeShells[ithread].end(),0);
     }
   }
 
   auto valVxc = [&](std::size_t iAtm, ChronusQ::IntegrationPoint &pt, 
-  KernelIntegrand<T> &result) -> void {
+    std::vector<KernelIntegrand<T>> &result) -> void {
 
+    int thread_id = omp_get_thread_num();
+
+    double *SCRATCH1DATA = SCRATCH1[thread_id].data();
+    double *SCRATCH1XDATA = SCRATCH1X[thread_id].data();
+    double *SCRATCH1YDATA = SCRATCH1Y[thread_id].data();
+    double *SCRATCH1ZDATA = SCRATCH1Z[thread_id].data();
+    double *OmegaADATA = OmegaA[thread_id].data();
+    double *OmegaBDATA = OmegaB[thread_id].data();
 
     cartGP &GP = pt.pt;
 
@@ -357,7 +420,7 @@ void SingleSlater<T>::formVXC_new(){
     if(doTimings) Newstart = std::chrono::high_resolution_clock::now();
     // For each new sphere, determine list of close basis shells (if screen ON)
     if(pt.I == 0 && this->screenVxc) {
-      closeShells.clear();
+      closeShells[thread_id].clear();
       double DX = (*this->molecule_->cart())(0,iAtm) - bg::get<0>(GP);
       double DY = (*this->molecule_->cart())(1,iAtm) - bg::get<1>(GP);
       double DZ = (*this->molecule_->cart())(2,iAtm) - bg::get<2>(GP);
@@ -371,7 +434,7 @@ void SingleSlater<T>::formVXC_new(){
         for(auto iSh = 0; iSh < this->basisset_->nShell(); iSh++){
           if(this->basisset_->mapSh2Cen(iSh)-1 != jAtm) continue;
           if(this->basisset_->radCutSh()[iSh] > DIST)
-            closeShells.push_back(iSh);
+            closeShells[thread_id].push_back(iSh);
         }
       }
     }
@@ -384,7 +447,7 @@ void SingleSlater<T>::formVXC_new(){
     if(doTimings) Newstart = std::chrono::high_resolution_clock::now();
 
     // Compute value of "close" basis functions at the given point
-    for(auto iShell : closeShells) {
+    for(auto iShell : closeShells[thread_id]) {
       int shSize= shSizes[iShell];
       int iSt = this->basisset_->mapSh2Bf(iShell);
       double * buff = this->basisset_->basisDEval(NDer,this->basisset_->shells(iShell),&pt.pt);
@@ -407,10 +470,10 @@ void SingleSlater<T>::formVXC_new(){
     if(doTimings) Newstart = std::chrono::high_resolution_clock::now();
 
     // Determine if we computed Zeros
-    double S1Norm = SCRATCH1.norm();
-    double S1XNorm = SCRATCH1X.norm();
-    double S1YNorm = SCRATCH1Y.norm();
-    double S1ZNorm = SCRATCH1Z.norm();
+    double S1Norm = SCRATCH1[thread_id].norm();
+    double S1XNorm = SCRATCH1X[thread_id].norm();
+    double S1YNorm = SCRATCH1Y[thread_id].norm();
+    double S1ZNorm = SCRATCH1Z[thread_id].norm();
 
     if(doTimings){
       Newend = std::chrono::high_resolution_clock::now();
@@ -425,15 +488,15 @@ void SingleSlater<T>::formVXC_new(){
     double rhoS(0.0);
     T Tt(0.0), Ts(0.0);
     T Pt(0.0), Ps(0.0);
-    GradRhoT.setZero();
-    GradRhoS.setZero();
+    GradRhoT[thread_id].setZero();
+    GradRhoS[thread_id].setZero();
 
     // Evaluate density and optionally density gradient at the given point
       
     if(doTimings) Newstart = std::chrono::high_resolution_clock::now();
     T * DENT, *DENS;
     // Loop over close shells "I"
-    for(auto iShell : closeShells) {
+    for(auto iShell : closeShells[thread_id]) {
       int iSz= shSizes[iShell];
       int iSt = this->basisset_->mapSh2Bf(iShell);
       for(auto iBf = iSt; iBf < (iSt + iSz); iBf++){
@@ -452,7 +515,7 @@ void SingleSlater<T>::formVXC_new(){
         
 
         // Loop over close shells "J"
-        for(auto jShell : closeShells) {
+        for(auto jShell : closeShells[thread_id]) {
           int jSz= shSizes[jShell];
           int jSt = this->basisset_->mapSh2Bf(jShell);
           for(auto jBf = jSt; jBf < (jSt + jSz); jBf++){
@@ -469,7 +532,7 @@ void SingleSlater<T>::formVXC_new(){
         } // jShell      
 
         if(this->nTCS_ == 1 && !this->isClosedShell){
-          for(auto jShell : closeShells) {
+          for(auto jShell : closeShells[thread_id]) {
             int jSz= shSizes[jShell];
             int jSt = this->basisset_->mapSh2Bf(jShell);
             for(auto jBf = jSt; jBf < (jSt + jSz); jBf++){
@@ -490,13 +553,13 @@ void SingleSlater<T>::formVXC_new(){
           rhoS += std::real(Ts) * SCRATCH1DATA[iBf];
         }  //UKS
         if(NDer > 0) {
-          drhoT[0] += std::real(Tt) * SCRATCH1XDATA[iBf];
-          drhoT[1] += std::real(Tt) * SCRATCH1YDATA[iBf];
-          drhoT[2] += std::real(Tt) * SCRATCH1ZDATA[iBf];
+          drhoT[thread_id][0] += std::real(Tt) * SCRATCH1XDATA[iBf];
+          drhoT[thread_id][1] += std::real(Tt) * SCRATCH1YDATA[iBf];
+          drhoT[thread_id][2] += std::real(Tt) * SCRATCH1ZDATA[iBf];
           if(this->nTCS_ == 1 && !this->isClosedShell){
-            drhoS[0] += std::real(Ts) * SCRATCH1XDATA[iBf];
-            drhoS[1] += std::real(Ts) * SCRATCH1YDATA[iBf];
-            drhoS[2] += std::real(Ts) * SCRATCH1ZDATA[iBf];
+            drhoS[thread_id][0] += std::real(Ts) * SCRATCH1XDATA[iBf];
+            drhoS[thread_id][1] += std::real(Ts) * SCRATCH1YDATA[iBf];
+            drhoS[thread_id][2] += std::real(Ts) * SCRATCH1ZDATA[iBf];
           } //UKS
         } //GGA
       } // iBf
@@ -508,14 +571,15 @@ void SingleSlater<T>::formVXC_new(){
 
     rhoT *= 0.5;
     rhoS *= 0.5;
-    rhoA = 0.5 * (rhoT + rhoS);
-    rhoB = 0.5 * (rhoT - rhoS);
+    double rhoA = 0.5 * (rhoT + rhoS);
+    double rhoB = 0.5 * (rhoT - rhoS);
+    double gammaAA,gammaBB,gammaAB;
     if( NDer > 0 ){
-      GradRhoA.noalias() = 0.5 * (GradRhoT + GradRhoS);
-      GradRhoB.noalias() = 0.5 * (GradRhoT - GradRhoS);
-      gammaAA = GradRhoA.dot(GradRhoA);           
-      gammaBB = GradRhoB.dot(GradRhoB);           
-      gammaAB = GradRhoA.dot(GradRhoB);           
+      GradRhoA[thread_id].noalias() = 0.5 * (GradRhoT[thread_id] + GradRhoS[thread_id]);
+      GradRhoB[thread_id].noalias() = 0.5 * (GradRhoT[thread_id] - GradRhoS[thread_id]);
+      gammaAA = GradRhoA[thread_id].dot(GradRhoA[thread_id]);           
+      gammaBB = GradRhoB[thread_id].dot(GradRhoB[thread_id]);           
+      gammaAB = GradRhoA[thread_id].dot(GradRhoB[thread_id]);           
     }
     
 //these if statements prevent numerical instability with zero guesses
@@ -546,50 +610,50 @@ void SingleSlater<T>::formVXC_new(){
     if(doTimings) Newstart = std::chrono::high_resolution_clock::now();
       if(NDer > 0 ) {
         for(auto iXYZ = 0; iXYZ < 3; iXYZ++){
-          double GA(GradRhoA(iXYZ)), GB(GradRhoB(iXYZ));
-            GradRhoA(iXYZ) = pt.weight * 
+          double GA(GradRhoA[thread_id](iXYZ)), GB(GradRhoB[thread_id](iXYZ));
+            GradRhoA[thread_id](iXYZ) = pt.weight * 
               ( 2.0 * GA * kernelXC.ddgammaAA + GB * kernelXC.ddgammaAB);
-            GradRhoB(iXYZ) = pt.weight * 
+            GradRhoB[thread_id](iXYZ) = pt.weight * 
               ( 2.0 * GB * kernelXC.ddgammaBB + GA * kernelXC.ddgammaAB);
         } // Grad
-        OmegaA.setZero(); 
-        for(auto iShell : closeShells) {
+        OmegaA[thread_id].setZero(); 
+        for(auto iShell : closeShells[thread_id]) {
           int iSz= shSizes[iShell];
           int iSt = this->basisset_->mapSh2Bf(iShell);
        
           for(auto iBf = iSt; iBf < (iSt + iSz); iBf++){
-            OmegaADATA[iBf] += GradRhoA(0) * SCRATCH1XDATA[iBf] +
-                               GradRhoA(1) * SCRATCH1YDATA[iBf] +
-                               GradRhoA(2) * SCRATCH1ZDATA[iBf];
+            OmegaADATA[iBf] += GradRhoA[thread_id](0) * SCRATCH1XDATA[iBf] +
+                               GradRhoA[thread_id](1) * SCRATCH1YDATA[iBf] +
+                               GradRhoA[thread_id](2) * SCRATCH1ZDATA[iBf];
           } // iBf
         } // iShell
         if(this->nTCS_ == 1 && !this->isClosedShell){
-          OmegaB.setZero(); 
-          for(auto iShell : closeShells) {
+          OmegaB[thread_id].setZero(); 
+          for(auto iShell : closeShells[thread_id]) {
              int iSz= shSizes[iShell];
              int iSt = this->basisset_->mapSh2Bf(iShell);
              for(auto iBf = iSt; iBf < (iSt + iSz); iBf++){
-                OmegaBDATA[iBf] += GradRhoB(0) * SCRATCH1XDATA[iBf] + 
-                                   GradRhoB(1) * SCRATCH1YDATA[iBf] + 
-                                   GradRhoB(2) * SCRATCH1ZDATA[iBf];  
+                OmegaBDATA[iBf] += GradRhoB[thread_id](0) * SCRATCH1XDATA[iBf] + 
+                                   GradRhoB[thread_id](1) * SCRATCH1YDATA[iBf] + 
+                                   GradRhoB[thread_id](2) * SCRATCH1ZDATA[iBf];  
              } // iBf
           } // iShell
         } //UKS
       } //GGA
-    result.Energy += pt.weight * kernelXC.eps;
+    result[thread_id].Energy += pt.weight * kernelXC.eps;
     kernelXC.ddrhoA *= pt.weight;
     kernelXC.ddrhoB *= pt.weight;
 
-    for(auto iShell : closeShells) {
+    for(auto iShell : closeShells[thread_id]) {
       int iSz= shSizes[iShell];
       int iSt = this->basisset_->mapSh2Bf(iShell);
 
       for(auto iBf = iSt; iBf < (iSt + iSz); iBf++){
         double Ta = kernelXC.ddrhoA*SCRATCH1DATA[iBf] + OmegaADATA[iBf];
-        for(auto jShell : closeShells) {
+        for(auto jShell : closeShells[thread_id]) {
           int jSz= shSizes[jShell];
           int jSt = this->basisset_->mapSh2Bf(jShell);
-          T *VXCDATA = result.VXCScalar.data() + iBf*this->nBasis_;
+          T *VXCDATA = result[thread_id].VXCScalar.data() + iBf*this->nBasis_;
           for(auto jBf = jSt; jBf < (jSt + jSz); jBf++){
             if(jBf < iBf) continue;
             VXCDATA[jBf] += Ta*SCRATCH1DATA[jBf] + SCRATCH1DATA[iBf]*OmegaADATA[jBf]; 
@@ -599,16 +663,16 @@ void SingleSlater<T>::formVXC_new(){
     } // iShell
 
     if(this->nTCS_ == 1 && !this->isClosedShell){
-      for(auto iShell : closeShells) {
+      for(auto iShell : closeShells[thread_id]) {
         int iSz= shSizes[iShell];
         int iSt = this->basisset_->mapSh2Bf(iShell);
   
         for(auto iBf = iSt; iBf < (iSt + iSz); iBf++){
           double Tb = kernelXC.ddrhoB*SCRATCH1DATA[iBf] + OmegaBDATA[iBf];
-          for(auto jShell : closeShells) {
+          for(auto jShell : closeShells[thread_id]) {
             int jSz= shSizes[jShell];
             int jSt = this->basisset_->mapSh2Bf(jShell);
-            T *VXCDATB = result.VXCMz.data() + iBf*this->nBasis_;
+            T *VXCDATB = result[thread_id].VXCMz.data() + iBf*this->nBasis_;
             for(auto jBf = jSt; jBf < (jSt + jSz); jBf++){
               if(jBf < iBf) continue;
               VXCDATB[jBf] += Tb*SCRATCH1DATA[jBf] + SCRATCH1DATA[iBf]*OmegaBDATA[jBf]; 
@@ -645,12 +709,12 @@ void SingleSlater<T>::formVXC_new(){
       static_cast<ChronusQ::ATOMIC_PARTITION>(this->weightScheme_),this->molecule_->cartArray(),
       this->molecule_->rIJ(),0,this->epsScreen,1e6,1.0,false);
 //NOMAG  KernelIntegrand<double> res(this->vXA_->cols());
-  KernelIntegrand<T> res(this->vXCScalar_->cols());
   //Screaning based on cutoff
 //  the radCutoof vector is populated in singleSlater_real_guess.cpp
 
   this->energyExc    = 0.0;
-  this->vXCScalar_->setZero();   // Set to zero every occurence of the SCF
+  std::vector<KernelIntegrand<T>> res(nthreads,
+    KernelIntegrand<T>(this->vXCScalar_->cols()));
 
   std::vector<double> atomRadCutoff(this->molecule_->nAtoms(),0.0);
 
@@ -664,6 +728,8 @@ void SingleSlater<T>::formVXC_new(){
       }
     } 
   }
+
+
   for(auto iAtm = 0; iAtm < this->molecule_->nAtoms(); iAtm++){
     AGrid.center() = iAtm;
 //    cout << "Cutoff Rad = " << atomRadCutoff[iAtm] << "for iAt= " << iAtm << endl;
@@ -676,24 +742,43 @@ void SingleSlater<T>::formVXC_new(){
     AGrid.scalingFactor()=0.5 *
       elements[this->molecule_->index(iAtm)].sradius/phys.bohr;
 
-    std::function<void(IntegrationPoint&,KernelIntegrand<T>&)>
+    std::function<void(IntegrationPoint&,std::vector<KernelIntegrand<T>>&)>
       wrapper = std::bind(valVxc,iAtm,std::placeholders::_1,
                   std::placeholders::_2);
 
-    AGrid.integrate<KernelIntegrand<T>>(wrapper,res);
+    AGrid.integrate<std::vector<KernelIntegrand<T>>>(wrapper,res);
   };
-//MAG   (*this->vXCScalar_) = 4*math.pi*res.VXCScalar;
-  if(this->isClosedShell && this->nTCS_ != 2){
-    (*this->vXCScalar_) = 8.0*math.pi*res.VXCScalar;
-  }else if(!this->isClosedShell && this->nTCS_ != 2){
-//MAG    (*this->vXCMz_) = 4*math.pi*res.VXCMz;
-    (*this->vXCScalar_) = 4.0*math.pi*(res.VXCScalar+res.VXCMz);  //ALPHA LIKE
-//    (*this->vXCMz_) = 4.0*math.pi*res.VXCMz;
-    (*this->vXCMz_) = 4.0*math.pi*(res.VXCScalar-res.VXCMz);      //BETA LIKE
-    (*this->vXCMz_) = this->vXCMz_->template selfadjointView<Lower>();
+
+
+
+  this->vXCScalar_->setZero();   // Set to zero every occurence of the SCF
+  this->vXCMz_->setZero();   // Set to zero every occurence of the SCF
+
+  
+  for(auto ithread = 0; ithread < nthreads; ithread++){
+    if(this->isClosedShell && this->nTCS_ != 2){
+      (*this->vXCScalar_) = 8.0*math.pi*res[ithread].VXCScalar;
+    } else if(!this->isClosedShell && this->nTCS_ != 2){
+      (*this->vXCScalar_) = 4.0*math.pi*(res[ithread].VXCScalar+res[ithread].VXCMz);  //ALPHA LIKE
+      (*this->vXCMz_) = 4.0*math.pi*(res[ithread].VXCScalar-res[ithread].VXCMz);      //BETA LIKE
+    }
+    this->energyExc = 4*math.pi*res[ithread].Energy;
   }
   (*this->vXCScalar_) = this->vXCScalar_->template selfadjointView<Lower>();
-  this->energyExc = 4*math.pi*res.Energy;
+  (*this->vXCMz_) = this->vXCMz_->template selfadjointView<Lower>();
+
+  // Cleanup Scratch
+  for(auto ithread = 0; ithread < nthreads; ithread++) {
+    this->memManager_->free(SCRATCH1[ithread].data(),this->nBasis_);
+    if(this->isGGA) {
+      this->memManager_->free(SCRATCH1X[ithread].data(),this->nBasis_);
+      this->memManager_->free(SCRATCH1Y[ithread].data(),this->nBasis_);
+      this->memManager_->free(SCRATCH1Z[ithread].data(),this->nBasis_);
+    }
+
+    this->memManager_->free(OmegaA[ithread].data(),this->nBasis_);
+    this->memManager_->free(OmegaB[ithread].data(),this->nBasis_);
+  }
 
   if(doTimings) {
     cout << "T1 = " << T1.count() << endl;
