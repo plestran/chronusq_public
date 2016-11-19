plestran/chronusq_public

commit 1c189b12abed56246a76dc98cb930b6dd8974972

Added a few more timers

include/singleslater/singleslater_fock.h
@@ -200,6 +200,7 @@ void SingleSlater<T>::formFock(bool increment){
 */
 
     if(!increment) {
+/*
       this->fockScalar_->setZero();
 
       if(this->nTCS_ == 2 or !this->isClosedShell)
@@ -209,7 +210,9 @@ void SingleSlater<T>::formFock(bool increment){
         this->fockMy_->setZero();
         this->fockMx_->setZero();
       }
+*/
 
+      auto AddHStart = std::chrono::high_resolution_clock::now();
       (*this->fockScalar_) = this->aointegrals_->coreH_->template cast<T>();
       this->aointegrals_->subElecDipole(*this->fockScalar_,this->elecField_);
 
@@ -230,8 +233,13 @@ void SingleSlater<T>::formFock(bool increment){
         (*this->fockMz_) *= ComplexScale<T>();
         // -----------------------------------
       }
+
       for(auto iF = fock_.begin(); iF != fock_.end(); iF++)
         *(*iF) *= 2;
+
+      auto AddHEnd = std::chrono::high_resolution_clock::now();
+
+      this->avgAddHD_ += AddHEnd - AddHStart;
     }
 
 /*
@@ -242,11 +250,14 @@ void SingleSlater<T>::formFock(bool increment){
         this->fockScalar_->data()[i] += this->PTScalar_->data()[i];
       }
 */
+    auto AddGPStart = std::chrono::high_resolution_clock::now();
     for(auto iF = 0; iF < fock_.size(); iF++) {
       *(fock_[iF]) += *(PT_[iF]);
       if(this->isDFT)
         (*fock_[iF]) += (*vXC_[iF]);
     }
+    auto AddGPEnd = std::chrono::high_resolution_clock::now();
+    this->avgAddGPD_ += AddGPEnd - AddGPStart;
 /*
     (*this->fockScalar_) += (*this->PTScalar_);
 */

include/singleslater.h
@@ -385,6 +385,8 @@ class SingleSlater : public WaveFunction<T> {
   std::chrono::duration<double> avgDiagD_;
   std::chrono::duration<double> avgCommD_;
   std::chrono::duration<double> avgOrthoD_;
+  std::chrono::duration<double> avgAddHD_;
+  std::chrono::duration<double> avgAddGPD_;
   
   
 

include/singleslater/singleslater_print.h
@@ -586,6 +586,16 @@ void SingleSlater<T>::printTimings() {
     }
 
     this->fileio_->out << std::left << std::setw(60) <<
+      "  Average Wall time for adding H:";
+    this->fileio_->out << std::left << std::setw(15) 
+      << this->avgAddHD_.count() << " sec" << endl;
+
+    this->fileio_->out << std::left << std::setw(60) <<
+      "  Average Wall time for adding G[P]/VXC:";
+    this->fileio_->out << std::left << std::setw(15) 
+      << this->avgAddGPD_.count() << " sec" << endl;
+
+    this->fileio_->out << std::left << std::setw(60) <<
       "Average Wall time for Fock diagonalization:";
     this->fileio_->out << std::left << std::setw(15) 
       << this->avgDiagD_.count() << " sec" << endl;

include/singleslater/singleslater_scf.h
@@ -68,6 +68,10 @@ void SingleSlater<T>::SCF3(){
   avgDiagD_  -= avgDiagD_;
   avgCommD_  -= avgCommD_;
   avgOrthoD_ -= avgOrthoD_;
+  avgVXCD_   -= avgVXCD_; 
+  avgPTD_    -= avgPTD_ ; 
+  avgAddHD_    -= avgAddHD_ ; 
+  avgAddGPD_    -= avgAddGPD_ ; 
 
   auto scfStart = std::chrono::high_resolution_clock::now();
   for(iter = 0; iter < this->maxSCFIter_; iter++){
@@ -227,6 +231,8 @@ void SingleSlater<T>::SCF3(){
   avgOrthoD_ /= nSCFIter; 
   avgVXCD_ /= nSCFIter; 
   avgPTD_ /= nSCFIter; 
+  avgAddHD_ /= nSCFIter; 
+  avgAddGPD_ /= nSCFIter; 
   
 
 

commit fb48310776273013f9e755e2a3d59f8665932eb2

Fixed bug introduced in 1c189b12abed56246a76dc98cb930b6dd8974972

include/singleslater/singleslater_fock.h
@@ -200,7 +200,6 @@ void SingleSlater<T>::formFock(bool increment){
 */
 
     if(!increment) {
-/*
       this->fockScalar_->setZero();
 
       if(this->nTCS_ == 2 or !this->isClosedShell)
@@ -210,7 +209,6 @@ void SingleSlater<T>::formFock(bool increment){
         this->fockMy_->setZero();
         this->fockMx_->setZero();
       }
-*/
 
       auto AddHStart = std::chrono::high_resolution_clock::now();
       (*this->fockScalar_) = this->aointegrals_->coreH_->template cast<T>();